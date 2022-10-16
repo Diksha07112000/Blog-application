551d3e9d15e74c0689000076c19d798eb4cc761b
Diksha07112000 <goyaldiksha0711@gmail.com>
2022-10-16 14:37:06 +0530

    Created User APIs

--- demo/src/main/java/com/diksha/blog/demo/controllers/UserController.java
+++ demo/src/main/java/com/diksha/blog/demo/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.diksha.blog.demo.controllers;
 
+import com.diksha.blog.demo.payloads.ApiResponse;
 import com.diksha.blog.demo.payloads.UserDto;
 import com.diksha.blog.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//make apis for all menthods
+import java.util.List;
+import java.util.Map;
+
+//make apis for all methods
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,6 +23,7 @@ public class UserController {
 
     //Post- create user
 
+//    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
     {
@@ -26,9 +31,41 @@ public class UserController {
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
     //Put-update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer uid)
+    {
+        UserDto updatedUserDto = this.userService.updateUser(userDto, uid);
+        return ResponseEntity.ok(updatedUserDto);
+    }
+
 
     //Delete-delete user
 
-    //Get - get user
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid)
+    {
+        this.userService.deleteUser(uid);
+        //1) for sending a message without status code
+        // return ResponseEntity.ok(Map.of("message","User Deleted Successfully"));
+        //2) for sending message with status code
+        // return new ResponseEntity(Map.of("message","User Deleted Successfully"),HttpStatus.OK);
 
+        //3) we are creating separate class for ApiResponse
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted",true),HttpStatus.OK);
+
+    }
+    //GetAll - get all users
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    //Get Single user
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId){
+        return ResponseEntity.ok(this.userService.getUserById(userId));
+    }
+ 
 }
---
+++ demo/src/main/java/com/diksha/blog/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.diksha.blog.demo.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
